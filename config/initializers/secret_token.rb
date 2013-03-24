6da479d619e1ae8b27154c5f0968954b7b40290d
Juan Mercedes <j.mercedes@thedesignpill.com>
2013-03-24 16:14:48 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drlopezcollado::Application.config.secret_token = 'd34743dba6f98c80203810e7388be9b5b99384cd4f7ea5a7a3e9069d68a811604cc9ae799c7d3253af94b7a976dcd2b057bdf1e6e474603d219a304831b3ba60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Drlopezcollado::Application.config.session_store :cookie_store, key: '_drlopezcollado_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Drlopezcollado::Application.config.session_store :active_record_store
